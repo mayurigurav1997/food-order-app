bd654f05a46836164f22fb397ec92e69cfed8c0c
Mayuri <mayurigurav1997@gmail.com>
2024-05-27 21:03:21 +0530

    Header is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <h1>Hello</h1>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-// import { logoImg } from "../assets/logo.jpg";
+import logoImg from "../assets/logo.jpg";
 export default function Header() {
   return (
     <header id="main-header">
       <div id="title">
-        {/* <img src={logoImg} alt="A Resturant" /> */}
+        <img src={logoImg} alt="A Resturant" />
         <h1>React Food</h1>
       </div>
       <nav>
